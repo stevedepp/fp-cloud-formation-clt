75e79e46b2ef32778ff6a5b5eb29cc2064be8c99
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-11-06 00:38:37 -0500

    dropped an empty line

--- 2-build-layer.sh
+++ 2-build-layer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -eo pipefail
+
 rm -rf packageServerlessProducer
 rm -rf packageProducerAI
+
 pip install --target ./packageServerlessProducer/python -r ./ServerlessProducer/requirements.txt
 pip install --target ./packageProducerAI/python -r ./ProducerAI/requirements.txt
 
