014c4c182b006a2f6e23a2545eb88d8b62ef49e7
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-11-06 01:00:46 -0500

    correcting comments in template

--- 4-init-data.sh
+++ 4-init-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./ddb_init.py
-aws dynamodb batch-write-item --request-items file://text.json
+./ddb_init.py > request.json
+aws dynamodb batch-write-item --request-items file://request.json
