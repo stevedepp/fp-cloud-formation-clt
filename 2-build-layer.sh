67a25ce8cdb65799bc39f08378d892c13691ac8a
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-11-23 12:20:07 -0500

    added --use-feature=2020-resolver to requirements.txt installs per msg in notes of README.md

--- 2-build-layer.sh
+++ 2-build-layer.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 rm -rf packageServerlessProducer
 rm -rf packageProducerAI
 
-pip install --target ./packageServerlessProducer/python -r ./ServerlessProducer/requirements.txt
-pip install --target ./packageProducerAI/python -r ./ProducerAI/requirements.txt
+pip install --target ./packageServerlessProducer/python -r ./ServerlessProducer/requirements.txt --use-feature=2020-resolver
+pip install --target ./packageProducerAI/python -r ./ProducerAI/requirements.txt --use-feature=2020-resolver
 
 rm -rf ./packageProducerAI/python/pandas
 rm -rf ./packageProducerAI/python/numpy
