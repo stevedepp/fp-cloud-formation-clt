42b6326c12931e0aa286efe52f06c6bca3ea718d
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-11-23 22:44:44 -0500

    adding func to cfcli.py

--- cfcli.py
+++ cfcli.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 import click
+import lib
 import sys
-from ddb_items import add_batch
-from ddb_items import add_item
+import subprocess
+#from ddb_items import add_batch
+#from ddb_items import add_item
 
-@click.version_option(nlib.__version__)
+@click.version_option(lib.__version__)
 @click.group()
 def cli():
     '''blah blah'''
 
-@cli.command('')
+@cli.command("make_infra")
+def hello():
+    print('hello')
+    subprocess.run(['make', 'env'])
 
 @cli.command("names")
-@click.option('--file', help='Column of names')
-@click.option('--name', help='Individual name')
+@click.option('--file', help='File containig a column of names')
+@click.option('--name', '-n', multiple=True, help='One name or several separated by -n')
+def hello(file, name):
+    if not file and not name:
+        click.echo("--file or --name is required")
+    elif file:
+        with open(file) as fin:
+            read_data = fin.read()
+        ret = "add_batch(file)"
+        click.echo(f"Processing batch @ {file}. \n--> adding: \n{read_data}")
+    elif name:
+        ret = "add_batch(file)"
+        click.echo('\n'.join(name))
+
+
+'''
 def names(file, name):
     if not file and not name:
         click.echo("--file or --name is required")
@@ -24,6 +43,7 @@ def names(file, name):
     elif name:
         ret = add_item(name)
         click.echo(f"Processing item {item} --> adding {ret}")
+'''
 
 if __name__ == "__main__":
     cli()
